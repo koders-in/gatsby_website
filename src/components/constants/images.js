c62e5163f4407bac7ef14506b4769ee958c73aa8
Smodi16 <99smodi@gmail.com>
2020-05-26 16:16:59 +0530

    Image Modified

--- src/components/constants/images.js
+++ src/components/constants/images.js
@@ -1,6 +1,6 @@
 import unity from '../../images/unity.png';
 import parking from '../../images/parking.png';
-import rusk from '../../images/rusk.png';
+import rusk from '../../images/Rusk.png';
 import Powershell from '../../images/Powershell.png';
 import Cloudsim from '../../images/Cloudsim.png';
 import Fraud from '../../images/Fraud.png';
